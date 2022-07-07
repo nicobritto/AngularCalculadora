12b18c3cada2c0c2de7515c5952b964185d19741
nico britto <nicobritto21@gmail.com>
2022-07-07 13:01:14 -0300

    calculadora sumando Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mi-Calculadora-Nico';
+
+  titulo="Aplicacion Calculara Nico";
+  operandoA:number=0;
+  operandoB:number=0;
+  resultado:number=0;
+
+
+  sumar():void{
+    this.resultado=this.operandoA+this.operandoB;
+  }
 }
